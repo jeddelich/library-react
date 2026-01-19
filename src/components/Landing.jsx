b27e1f1e29f64b3952d2a7563007148ff3e128ca
jeddelich@gmail.com <jeddelich@gmail.com>
2026-01-18 23:58:07 -0500

    library-react-connected

---
+++ src/components/Landing.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Undraw_Books from '../assets/Undraw_Books.svg';
+
+function Landing() {
+  return (
+    <section id="landing">
+      <header>
+        <div className="header__container">
+          <div className="header__description">
+            <h1>America's most awarded online library platform</h1>
+            <h2>
+              Find your dream book with <span className="purple">Library</span>
+            </h2>
+            <a href="#features">
+              <button className="btn">Browse books</button>
+            </a> 
+          </div>
+          <figure className="header__img--wrapper">
+            <img src={Undraw_Books} alt="" />
+          </figure>
+        </div>
+      </header>
+    </section>
+  );
+}
+
+export default Landing;
